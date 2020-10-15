d33a090d7ac74abe2f9d3206beb632bfe262b70a
Qiao-Ling CHOU <pamela81123@gmail.com>
2020-10-15 23:40:49 +0200

    select the reviews last than 100 words

---
+++ select.py
@@ -0,0 +1,16 @@
+data = []
+with open ('reviews.txt', 'r') as f:
+	for line in f:
+		data.append(line)
+print('總用有',len(data), '筆留言')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+print('留言平均長度為', sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('總用有', len(new), '筆留言的字數小於100')
